ae0a9f69220aba889ad1dd2163afaa54cc9f0240
sadia554466 <pue.khulna@gmail.com>
2022-11-02 01:46:52 +0600

    Food Pathao

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+$(".info").slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+});
+
+
+$(".testi-item").slick({
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+     autoplaySpeed: 1000,
+});
+$('.counter').counterUp({
+                delay: 10,
+                time: 1000
+            });
